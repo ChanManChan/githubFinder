9503cd2bb3f334b0f19ed00bef84645cee51d74b
ChanManChan <42992965+ChanManChan@users.noreply.github.com>
2020-01-12 09:33:06 +0530

    Add files via upload
    github Finder without Hooks

---
+++ precache-manifest.16c9dc844624ff822d1962addbfaa472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a42339667499b381d18cd237cba767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e0fc9dbbc6d72c0ba91",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "3e8eb96551d98c21e33e",
+    "url": "/static/js/2.0ab5ac27.chunk.js"
+  },
+  {
+    "revision": "9b177b59f5d3908ffc5ba46d5eb0523f",
+    "url": "/static/js/2.0ab5ac27.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e0fc9dbbc6d72c0ba91",
+    "url": "/static/js/main.c8837495.chunk.js"
+  },
+  {
+    "revision": "08e32aa381635ae8ad79",
+    "url": "/static/js/runtime-main.622db41a.js"
+  }
+]);
